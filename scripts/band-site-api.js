fff9285631290852032d4231636ea27deab7b8ff
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-01-14 19:55:57 -0800

    fetch data from api and show it on ui

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,61 +1,61 @@
 // const apiKey = "0c1318a1-0c91-4df5-9cfa-b8cbad39d045"
 
-// API comment: post, get and put 
+// API comment: post, get and put
 // https://project-1-api.herokuapp.com/comments?api_key=0c1318a1-0c91-4df5-9cfa-b8cbad39d045
 
-// API shows: get 
+// API shows: get
 // https://project-1-api.herokuapp.com/showdates?api_key=0c1318a1-0c91-4df5-9cfa-b8cbad39d045
 
 class BandSiteApi {
-    constructor(apiKey){
-        this.apiKey = "0c1318a1-0c91-4df5-9cfa-b8cbad39d045",
-        this.baseUrl = "https://project-1-api.herokuapp.com/"
-    }
+  constructor(apiKey) {
+    this.apiKey = "0c1318a1-0c91-4df5-9cfa-b8cbad39d045",
+    this.baseUrl = "https://project-1-api.herokuapp.com/"
+  }
 
-    // PostComment object
-    postComment = async (comment) => {
-        const url = 'https://project-1-api.herokuapp.com/comments?api_key=0c1318a1-0c91-4df5-9cfa-b8cbad39d045'
-                
-        const userData = {
-            name: comment.name,
-            comment: comment.comment,
-        }
-        try{
-            const response = await axios.post(url, userData)
-            const finalOutput = response.data
-            return finalOutput
-        } catch(error){
-            console.error('Error PostComment: ', error)
-        }
+  // PostComment object
+  postComment = async (comment) => {
+    const url = `${this.baseUrl}comments?api_key=${this.apiKey}`;
+    const userData = {
+      name: comment.name,
+      comment: comment.comment,
+    };
+    try {
+      const response = await axios.post(url, userData);
+      const finalOutput = response.data;
+      return finalOutput;
+    } catch (error) {
+      console.error("Error PostComment: ", error);
     }
+  };
 
-    // GetComments object
-    getComments = async () => {
-        const url = 'https://project-1-api.herokuapp.com/comments?api_key=0c1318a1-0c91-4df5-9cfa-b8cbad39d045'
-        try{
-            const response = await axios.get(url)
-            const finalOutput = response.data
-            // Sort comments array based on timestamp
-            const sortedCommentsArray = finalOutput.sort((a,b) => a.timestamp - b.timestamp)
-            return sortedCommentsArray
-        } catch(error){
-            console.error('Error GetComment: ', error)
-        }
+  // GetComments object
+  getComments = async () => {
+    const url = `${this.baseUrl}comments?api_key=${this.apiKey}`;
+    try {
+      const response = await axios.get(url);
+      const finalOutput = response.data;
+      // Sort comments array based on timestamp
+      const sortedCommentsArray = finalOutput.sort(
+        (a, b) => a.timestamp - b.timestamp
+      );
+      return sortedCommentsArray;
+    } catch (error) {
+      console.error("Error GetComment: ", error);
     }
+  };
 
-    // GetShows object
-    getShows = async () => {
-        const url = 'https://project-1-api.herokuapp.com/showdates?api_key=0c1318a1-0c91-4df5-9cfa-b8cbad39d045'
-        try{
-            const response = await axios.get(url)
-            const finalOutput = response.data
-            return finalOutput
-        } catch(error){
-            console.error('Error GetShows: ', error)
-        }
+  // GetShows object
+  getShows = async () => {
+    const url = `${this.baseUrl}showdates?api_key=${this.api}`
+    try {
+      const response = await axios.get(url);
+      const finalOutput = response.data;
+      console.log(finalOutput)
+      return finalOutput;
+    } catch (error) {
+      console.error("Error GetShows: ", error);
     }
+  };
 }
 
 export default BandSiteApi;
-
-  
--- scripts/buid-shows-page.js
+++ scripts/buid-shows-page.js
@@ -1,130 +1,131 @@
-import BandSiteApi from "./band-site-api.js"
+import BandSiteApi from "./band-site-api.js";
 
-const showHeading = ["DATE", "VENUE", "LOCATION"
-]
+const apiKey = "0c1318a1-0c91-4df5-9cfa-b8cbad39d045";
+const bandSiteApi = new BandSiteApi(apiKey);
 
-const showsList = [
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    }
-]
+const showHeading = ["DATE", "VENUE", "LOCATION"];
 
-  
-  // Function to create and render show elements
-  function renderShows() {
-    const showsContainer = document.querySelector('.shows-container');
+// const showsList = [
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     }
+// ]
+
+// Function to create and render show elements
+const renderShows = async () => {
+  try {
+    const showsList = await bandSiteApi.getShows();
+
+    const showsContainer = document.querySelector(".shows-container");
     // Create elements
     // Containing single DATE, VENUE, LOCATION headings only for tablet and destop screen
-    const headingsCardEl = document.createElement('div')
+    const headingsCardEl = document.createElement("div");
 
-    const headingDateEl = document.createElement('p')
-    const headingVenueEl = document.createElement('p')
-    const headingLocationEl = document.createElement('p')
-    const headingButtonEl = document.createElement('button')
+    const headingDateEl = document.createElement("p");
+    const headingVenueEl = document.createElement("p");
+    const headingLocationEl = document.createElement("p");
+    const headingButtonEl = document.createElement("button");
 
     // Add Classes for headings
-    headingsCardEl.classList.add('headings-card')
-    headingDateEl.classList.add('headings-card__date')
-    headingVenueEl.classList.add('headings-card__venue')
-    headingLocationEl.classList.add('headings-card__location')
-    headingButtonEl.classList.add('button')
-    headingButtonEl.classList.add('headings-card__button')
+    headingsCardEl.classList.add("headings-card");
+    headingDateEl.classList.add("headings-card__date");
+    headingVenueEl.classList.add("headings-card__venue");
+    headingLocationEl.classList.add("headings-card__location");
+    headingButtonEl.classList.add("button");
+    headingButtonEl.classList.add("headings-card__button");
     // Set InnerText
-    headingDateEl.innerText = 'DATE'
-    headingVenueEl.innerText = 'VENUE'
-    headingLocationEl.innerText = 'LOCATION'
-    headingButtonEl.innerText = 'BUY TICKETS'
+    headingDateEl.innerText = "DATE";
+    headingVenueEl.innerText = "VENUE";
+    headingLocationEl.innerText = "LOCATION";
+    headingButtonEl.innerText = "BUY TICKETS";
 
     // Append Child to Parent
-    headingsCardEl.appendChild(headingDateEl)
-    headingsCardEl.appendChild(headingVenueEl)
-    headingsCardEl.appendChild(headingLocationEl)
-    headingsCardEl.appendChild(headingButtonEl)
-    showsContainer.appendChild(headingsCardEl)
+    headingsCardEl.appendChild(headingDateEl);
+    headingsCardEl.appendChild(headingVenueEl);
+    headingsCardEl.appendChild(headingLocationEl);
+    headingsCardEl.appendChild(headingButtonEl);
+    showsContainer.appendChild(headingsCardEl);
 
     showsList.forEach((show) => {
-    // Create elements
-      const showCardEl = document.createElement('div')
-      const dateHeadingEl = document.createElement('p')
-      const venueHeadingEl = document.createElement('p')
-      const locationHeadingEl = document.createElement('p')
-      const dateEl = document.createElement('p')
-      const venueEl = document.createElement('p')
-      const locationEl = document.createElement('p')
-      const ticketsButtonEl = document.createElement('button')
+      // Create elements
+      const showCardEl = document.createElement("div");
+      const dateHeadingEl = document.createElement("p");
+      const venueHeadingEl = document.createElement("p");
+      const locationHeadingEl = document.createElement("p");
+      const dateEl = document.createElement("p");
+      const venueEl = document.createElement("p");
+      const locationEl = document.createElement("p");
+      const ticketsButtonEl = document.createElement("button");
 
-    // Add classes
-      showCardEl.classList.add('shows-container__card')
-      dateHeadingEl.classList.add('shows-container__heading')
-      dateEl.classList.add('shows-container__date')
-      venueHeadingEl.classList.add('shows-container__heading')
-      venueEl.classList.add('shows-container__venue')
-      locationHeadingEl.classList.add('shows-container__heading')
-      locationEl.classList.add('shows-container__location')
-      ticketsButtonEl.classList.add('button')
+      // Add classes
+      showCardEl.classList.add("shows-container__card");
+      dateHeadingEl.classList.add("shows-container__heading");
+      dateEl.classList.add("shows-container__date");
+      venueHeadingEl.classList.add("shows-container__heading");
+      venueEl.classList.add("shows-container__venue");
+      locationHeadingEl.classList.add("shows-container__heading");
+      locationEl.classList.add("shows-container__location");
+      ticketsButtonEl.classList.add("button");
 
-
-    // Set innerText
-      dateHeadingEl.innerText = showHeading[0]
-      venueHeadingEl.innerText = showHeading[1]
-      locationHeadingEl.innerText = showHeading[2]
-      dateEl.innerText = show.date
-      venueEl.innerText = show.venue
-      locationEl.innerText = show.location
-      ticketsButtonEl.innerText = `BUY TICKETS`
+      // Set innerText
+      dateHeadingEl.innerText = showHeading[0];
+      venueHeadingEl.innerText = showHeading[1];
+      locationHeadingEl.innerText = showHeading[2];
+      dateEl.innerText = show.date;
+      venueEl.innerText = show.place;
+      locationEl.innerText = show.location;
+      ticketsButtonEl.innerText = `BUY TICKETS`;
 
       // append child to parent
-      showsContainer.appendChild(showCardEl)
-      showCardEl.appendChild(dateHeadingEl)
-      showCardEl.appendChild(dateEl)
-      showCardEl.appendChild(venueHeadingEl)
-      showCardEl.appendChild(venueEl)
-      showCardEl.appendChild(locationHeadingEl)
-      showCardEl.appendChild(locationEl)
-      showCardEl.appendChild(ticketsButtonEl)
+      showsContainer.appendChild(showCardEl);
+      showCardEl.appendChild(dateHeadingEl);
+      showCardEl.appendChild(dateEl);
+      showCardEl.appendChild(venueHeadingEl);
+      showCardEl.appendChild(venueEl);
+      showCardEl.appendChild(locationHeadingEl);
+      showCardEl.appendChild(locationEl);
+      showCardEl.appendChild(ticketsButtonEl);
 
-      // Add event listeners for clicked show effect 
-      showCardEl.addEventListener('click', () => {
-        document.querySelectorAll('.shows-container__card').forEach((card) => {
-          item.classList.remove('show__clicked');
-        });
- 
-        showCardEl.classList.add('show__clicked');
-      });
+      // Add event listeners for clicked show effect
+      // showCardEl.addEventListener("click", () => {
+      //   document.querySelectorAll(".shows-container__card").forEach((card) => {
+      //     item.classList.remove("show__clicked");
+      //   });
 
+      //   showCardEl.classList.add("show__clicked");
+      // });
     });
-
-    
+  } catch (error) {
+    console.error("Error render shows: ", error);
   }
-  
-  // Initial render
-  renderShows();
-  
+};
 
+// Initial render
+renderShows();
